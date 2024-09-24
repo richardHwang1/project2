46e777831e31e2c5104e6a2ad8ec630dca112b55
Richard Hwang <richard.hwang1@gmail.com>
2024-09-24 13:20:39 +0900

    Update ubuntu-nfs-server.sh

--- ubuntu-nfs-server.sh
+++ ubuntu-nfs-server.sh
@@ -5,5 +5,6 @@ echo "/nfs-shared 172.16.0.0/12(rw,sync)" >> /etc/exports
 systemctl restart nfs-server
 systemctl enable nfs-server
 apt install -y amazon-ec2-utils
+mkdir /nfs-shared
 ec2-metadata -o | cut -d ' ' -f 2 > /nfs-shared/list.txt
 chmod -R 777 /nfs-shared/
